81901f693ebfe23e4832a4cb05d733ed3dc12a19
Annie Huang <annie.hn@gmail.com>
2020-12-14 22:10:52 +1100

    Create the 404 page, including adding a link to the home page.

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom';
 const Error = () => {
   return (
     <Wrapper>
-      <h2>error page</h2>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, the page you tried cannot be found</h3>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+      </div>
     </Wrapper>
   );
 };
