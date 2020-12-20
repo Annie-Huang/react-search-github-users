eccdcc7f3670371187d8699c0c2d6cfdb25e1132
Annie Huang <annie.hn@gmail.com>
2020-12-21 10:36:25 +1100

    Get repos from GithubContext in Repos.js and confirmed we can get it.

--- src/context/context.js
+++ src/context/context.js
@@ -12,11 +12,11 @@ const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
-  const [repo, setRepo] = useState(mockRepos);
+  const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
   return (
-    <GithubContext.Provider value={{githubUser, repo, followers}}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{githubUser, repos, followers}}>{children}</GithubContext.Provider>
   );
 };
 
