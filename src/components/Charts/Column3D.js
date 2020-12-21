b045fd999f14f5e1c6f750e02b29c7085aaa2367
Annie Huang <annie.hn@gmail.com>
2020-12-21 21:27:11 +1100

    Copy the content of Pie3D.js into Columns3D.js

--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -1,7 +1,44 @@
+// STEP 1 - Include Dependencies
+// Include react
 import React from 'react';
 
-const Column3D = () => {
-  return <div>chart</div>;
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts';
+
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Include the chart type
+import Chart from 'fusioncharts/fusioncharts.charts';
+
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: 'pie3d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: 'Languages',
+        theme: 'fusion',
+        decimals: 0,
+        pieRadius: '35%', // make the pie bigger, if you set it to too big, depend on the data comes in, the text, e.g. Javascript, 32% will be outside the <Wrapper> container.
+        // paletteColors: '#f0db4f',
+        // paletteColors: ['#5d62b5', '#afafaf', '#f2726', '#ffc533', '#62b58f', '#afafaf'],
+      },
+      // Chart Data
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Column3D;
+export default ChartComponent;
