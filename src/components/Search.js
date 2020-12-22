e58c64bda4f0c05f9ff269bffcb4866a599e3d3d
Annie Huang <annie.hn@gmail.com>
2020-12-22 15:29:15 +1100

    Get the requests from appContext in Search.js and displays it on the page.

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,8 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [user, setUser] = React.useState('');
+  const { requests } = React.useContext(GithubContext);
+  console.log(requests);
 
   // get things from global context
   const handleSubmit = (e) => {
@@ -30,7 +32,7 @@ const Search = () => {
             <button type='submit'>search</button>
           </div>
         </form>
-        <h3>requests: 60 / 60</h3>
+        <h3>requests: {requests} / 60</h3>
       </Wrapper>
     </section>
   );
