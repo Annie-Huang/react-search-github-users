e454a6fcb61a24dc479556afd21e28a9bf425d2b
Annie Huang <annie.hn@gmail.com>
2020-12-22 11:22:55 +1100

    Setup local variable requests, loading. and useEffect for page onload in context.js

--- src/context/context.js
+++ src/context/context.js
@@ -15,9 +15,22 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [loading, setIsLoading] = useState(false);
+  
+  // check rate
+
+  // error
+  useEffect(() => {
+    console.log('hey, app loaded');
+  }, []);
+
   return (
-    <GithubContext.Provider value={{githubUser, repos, followers}}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
-export {GithubProvider, GithubContext};
+export { GithubProvider, GithubContext };
