f0238cb5821e9450eb963373f95bce6f2f759fed
Annie Huang <annie.hn@gmail.com>
2020-12-23 19:13:31 +1100

    Follow https://auth0.com/docs/libraries/auth0-react to set up <Auth0Provider> to wrap around <App>

--- src/index.js
+++ src/index.js
@@ -6,11 +6,23 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+/* 
+From Auth0:
+Domain: dev-9389el5o.au.auth0.com
+Client ID: Tmg4a49Kay86YE5ZMc51UIkRFJFEEQQg
+https://auth0.com/docs/libraries/auth0-react
+*/
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-9389el5o.au.auth0.com'
+      clientId='Tmg4a49Kay86YE5ZMc51UIkRFJFEEQQg'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
